cecbac546aa58d8972550fdc71d2e52551910c54
boris <bnikitenko@yandex.ru>
2024-05-26 17:44:47 +0600

    homework#2

---
+++ Homework#2/todo_move_me_out.cpp
@@ -0,0 +1,7 @@
+#include "todo_move_me_out.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file. And Mr.Boris has done the thing!" << std::endl;
+}
