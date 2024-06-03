b79e1adb2d353c684aa2c0880cd1f887bc92449b
boris <bnikitenko@yandex.ru>
2024-06-03 21:48:11 +0600

    renemed dir

---
+++ Homework2/Otus-CPP-BN.cpp
@@ -0,0 +1,12 @@
+﻿// Otus-CPP-BN.cpp : Defines the entry point for the application.
+//
+
+#include "Otus-CPP-BN.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
